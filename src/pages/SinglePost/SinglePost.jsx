6dc2db61890ce6fed2748e3888493d2c1a3faf18
nahid7g <nahid.h.kiran@gmail.com>
2024-09-05 10:20:19 +0600

    Single Page updated

--- src/pages/SinglePost/SinglePost.jsx
+++ src/pages/SinglePost/SinglePost.jsx
@@ -1,14 +1,26 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { articles } from "../../constants";
 import Article from "./Article";
+import { FaArrowLeft } from "react-icons/fa";
 
 const SinglePost = () => {
   const { id } = useParams();
   const post = articles.find((article) => article.id == id);
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
   return (
     <div className="single-post">
       <div className="container">
         <div className="post-top">
+          <button
+            onClick={goBack}
+            className="omni-primary-btn d-flex align-items-center gap-2"
+          >
+            Go Back
+            <FaArrowLeft />
+          </button>
           <div className="article gap-top-45">
             <Article article={post} />
           </div>
